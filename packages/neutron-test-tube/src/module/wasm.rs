08dfacc158152ad6c7a158962b1f1048aac4e656
maxrobot <max.grabner@staedter.io>
2025-03-12 09:43:45 +0000

    chore: bump v5.1.3

--- packages/neutron-test-tube/src/module/wasm.rs
+++ packages/neutron-test-tube/src/module/wasm.rs
@@ -133,7 +133,7 @@ where
     pub fn query<M, Res>(&self, contract: &str, msg: &M) -> RunnerResult<Res>
     where
         M: ?Sized + Serialize,
-        Res: ?Sized + DeserializeOwned,
+        Res: DeserializeOwned,
     {
         let res = self
             .runner
